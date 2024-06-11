8ba9b91d74255a7f26853476f796d8bf08a4f66b
Buğra Uğur Toygar <151760316+Bugra-Ugur-Toygar@users.noreply.github.com>
2024-06-11 09:15:25 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import CCurency from './components/CCurency'
+
+
+function App() {
+ 
+
+
+
+  return (
+    <>
+   <div style={{display:'flex', flexDirection:'column', justifyItems:'center', alignItems: 'center'}}>
+    
+<CCurency/>
+
+       </div>
+    </>
+  )
+}
+
+export default App
